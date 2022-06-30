ef885afe7154758c485e676868f15d196b9cf176
aldopaganijr <aldojpagani@gmail.com>
2022-06-29 21:13:20 -0400

    styling for about me done

--- client-webpage/src/App.js
+++ client-webpage/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/_App.scss"
 import AboutUs from "./pages/AboutUs";
 
 function App() {
--- client-webpage/src/components/AboutSection.js
+++ client-webpage/src/components/AboutSection.js
@@ -1,33 +1,30 @@
 import React from "react";
-import home1 from "../images/home1.png"
+import home1 from "../images/me.jpg"
 
 const AboutSection = () => {
     return(
-        <div>
+        <div class="about">
             <div className="description">
                 <div className="title">
                     <div className="hide">
                         <h2>
-                            We work to make
+                            Less Human
                         </h2>
                     </div>
                     <div className="hide">
                         <h2>
-                            Your <span>dreams</span> come  
+                            More <span>Being.</span>   
                         </h2>
-                    </div>
-                    <div className="hide">
-                        <h2>true.</h2>
                     </div>
                 </div>
                 <p>Contact us for any photography or videography ideas that you have. We have some of the best creative filmmakers in the industry.</p>
                 <button>Contact Us</button>
             </div>
-            <div className="image">
-                <img src={home1} alt="guy with a camera" />
+            <div className="image1">
+                <img id="aldo" src={home1} alt="guy with a camera" />
             </div>
         </div>
     )
-}
+};
 
 export default AboutSection;
