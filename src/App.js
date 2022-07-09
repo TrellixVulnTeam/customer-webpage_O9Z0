c594077b10ae78a181f5db152f048bbec5434737
aldopaganijr <aldojpagani@gmail.com>
2022-07-09 09:34:11 -0400

    updates

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./styles/App.scss"
+import Photography from "./pages/Photography";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import {Switch, Route, useLocation}  from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
+
+
+
+function App() {
+
+  const location = useLocation();
+
+  return (
+
+          <div className="App">
+            <Nav/>
+            <AnimatePresence exitBeforeEnter>
+              <Switch location={location} key={location.pathname}>
+                <Route path="/" exact>
+                  <About/>
+                </Route>
+                <Route path="/Photography">
+                  <Photography/>
+                </Route>
+                <Route path="/Contact">
+                  <Contact/>
+                </Route>
+              </Switch>
+            </AnimatePresence>
+            <Footer />
+          </div>
+  
+  );
+}
+
+export default App;
+
